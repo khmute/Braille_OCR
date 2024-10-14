468251d1ecb41e5a9f8efd3a8e065106b6a0f0c9
khmute <khmute@gmail.com>
2024-10-15 03:22:55 +0900

    update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-# 의존성 설치 등
+# 일반 사용자 권한으로 실행됩니다.
+
 cd /home/ubuntu/app
 
-# 가상환경 생성 및 활성화 (선택 사항)
+# 가상환경 생성
 python3 -m venv venv
+
+# 가상환경 활성화
 source venv/bin/activate
 
+# pip 업그레이드
+pip install --upgrade pip
+
 # 의존성 설치
-pip3 install --upgrade pip
 if [ -f requirements.txt ]; then pip install -r requirements.txt; fi
