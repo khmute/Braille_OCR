abdaef1f579bbd6ac3780de234170a378d447f1c
khmute <khmute@gmail.com>
2024-10-15 03:22:27 +0900

    update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # 기존 애플리케이션 파일 삭제 또는 백업
-rm -rf /home/ubuntu/app/*
 
+# 패키지 목록 업데이트
 apt-get update
-apt-get install -y python3 python3-pip python3-venv
+
+# 필요한 패키지 설치
+apt-get install -y python3-full python3-pip python3-venv
+
+# 현재 사용자에게 /home/ubuntu/app 디렉토리에 대한 소유권 부여
+chown -R ubuntu:ubuntu /home/ubuntu/app
