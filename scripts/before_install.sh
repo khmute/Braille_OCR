ad03d2c1fe45bc6686eb49de42f3b51736a572f6
khmute <khmute@gmail.com>
2024-10-15 03:11:03 +0900

    update before_install hooks to install pip

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,7 +2,5 @@
 # 기존 애플리케이션 파일 삭제 또는 백업
 rm -rf /home/ubuntu/app/*
 
-hooks:
-    BeforeInstall:
-    - location: scripts/before_install.sh
-        runas: root
+apt-get update
+apt-get install -y python3 python3-pip python3-venv
